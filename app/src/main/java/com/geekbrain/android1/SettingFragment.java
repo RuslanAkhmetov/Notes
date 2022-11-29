8a6af3c12bd4e88f18c9a672858276bd9097e266
RuslanAkhmetov <akhmrusl@gmail.com>
2022-11-29 21:11:36 +0300

    Home task 8 add setting add to the end or to the start

--- app/src/main/java/com/geekbrain/android1/SettingFragment.java
+++ app/src/main/java/com/geekbrain/android1/SettingFragment.java
@@ -1,12 +1,21 @@
 package com.geekbrain.android1;
 
+
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+
+import com.geekbrain.android1.viewmodel.NotesViewModel;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +28,8 @@ public class SettingFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private static final String TAG = "Setting_Fragment";
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -60,5 +71,24 @@ public class SettingFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_setting, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initSettingFragment(view);
+    }
+
+    private void initSettingFragment(View view) {
+        NotesViewModel model = new ViewModelProvider(requireActivity()).get(NotesViewModel.class);
+        SwitchCompat addNoteSwitch = view.findViewById(R.id.add_note_switch);
+        addNoteSwitch.setChecked(model.isAddNewToTheEnd());
+        addNoteSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                model.setAddNewToTheEnd(b);
+                Log.i(TAG, "onCheckedChanged: " + model.isAddNewToTheEnd());
+            }
+        });
     }
 }
